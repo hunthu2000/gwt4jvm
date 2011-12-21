529c01c5e582b5b6c660302c554ed7a50ca5aec5
windicted <windicted@mind.com>
2011-12-22 00:46:39 +0400

    Remove Mind's copyright notice, the source hasn't been modified and restore original formating to simplify diffing.

--- gwt-java-client-overrides/src/main/java/com/google/gwt/core/client/GwtScriptOnly.java
+++ gwt-java-client-overrides/src/main/java/com/google/gwt/core/client/GwtScriptOnly.java
@@ -1,35 +1,18 @@
 /*
- * Copyright 2010 Mind Ltd.
- *
+ * Copyright 2008 Google Inc.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at:
  * 
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * 
- * This file incorporates work covered by the following copyright and permission
- * notice:  
- * 
- *     Copyright 2009 Google Inc.
- * 
- *     Licensed under the Apache License, Version 2.0 (the "License"); you may
- *     not use this file except in compliance with the License. You may obtain a
- *     copy of the License at:
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *     WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *     License for the specific language governing permissions and limitations
- *     under the License.
-*/
+ */
 package com.google.gwt.core.client;
 
 import java.lang.annotation.Documented;
@@ -54,4 +37,5 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Target({ElementType.TYPE, ElementType.METHOD})
-public @interface GwtScriptOnly {}
+public @interface GwtScriptOnly {
+}
