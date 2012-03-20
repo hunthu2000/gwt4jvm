hunthu2000/gwt4jvm

commit 38c51c6ebe99d35dd93c7df90529a0f638cbbd7d

Clean up and speed up GWT-RPC (de)serialization engine: introduce TypeHandlerFactory which caches instances of standard (generated by GWT) field serializers and ReflectiveTypeHandlers. The last now is used more widely, whereas standard field serializers are used only if serializable class has custom fiels serializer.

gwt-java-client/src/main/java/com/google/gwt/user/client/rpc/impl/TypeHandlerFactory.java
@@ -1,0 +1,101 @@
+/*
+ * Copyright 2012 Mind Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+*/
+package com.google.gwt.user.client.rpc.impl;
+
+import java.lang.reflect.Proxy;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import com.google.gwt.user.client.rpc.SerializationException;
+
+class TypeHandlerFactory
+{
+    private static final ConcurrentMap<Class<?>, TypeHandler> typeHandlers = new ConcurrentHashMap<Class<?>, TypeHandler>();
+
+    public static TypeHandler getTypeHandler(Class<?> c) throws SerializationException
+    {
+        try
+        {
+            if (!typeHandlers.containsKey(c))
+            {
+                typeHandlers.putIfAbsent(c, createTypeHandler(c));
+            }
+            return typeHandlers.get(c);
+        }
+        catch (Exception exception)
+        {
+            throw new SerializationException(exception);
+        }
+    }
+
+    /**
+     * Creates and returns thread-safe <tt>TypeHandler</tt> for the specified GWT-RPC serializable class. Standard
+     * (generated by GWT) field serializers are only used if the serializable class has {@link CustomFieldSerializer},
+     * otherwise an instance of {@link ReflectiveTypeHandler} is returned instead.
+     *
+     * <p>Standard field serializers (which don't rely on custom field serializers) aren't used not only because they
+     * can contain JSNI methods. Even if they don't, they still might delegate to "super field serializer" which is
+     * dedicated to (de)serialize fields of class that the serializable class is inherited from. Since the delegation
+     * is performed through a static call of <tt>serialize/deserialize</tt> method which can't be intercepted (easily)
+     * and which in its turn can call JSNI setters/getters, we limit the use of standard fields serializers.
+    */
+    private static TypeHandler createTypeHandler(Class<?> c) throws SerializationException
+    {
+        try
+        {
+            Class<TypeHandler> typeHandlerClass = getGeneratedTypeHandlerClass(c);
+            if (c.isEnum() || isDelegateToCustomFieldSerializer(typeHandlerClass))
+            {
+                return typeHandlerClass.getDeclaredConstructor().newInstance();
+            }
+            else
+            {
+                return (TypeHandler) Proxy.newProxyInstance(TypeHandler.class.getClassLoader(), new Class[] {TypeHandler.class}, new ReflectiveTypeHandler(c));
+            }
+        }
+        catch (Exception exception)
+        {
+            throw new SerializationException(exception);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Class<TypeHandler> getGeneratedTypeHandlerClass(Class<?> c) throws ClassNotFoundException
+    {
+        String generatedTypeHandlerClassName = c.getName().replace('$', '_') + "_FieldSerializer";
+        if (generatedTypeHandlerClassName.startsWith("java"))
+        {
+            generatedTypeHandlerClassName = "com.google.gwt.user.client.rpc.core." + generatedTypeHandlerClassName;
+        }
+        return (Class<TypeHandler>) Class.forName(generatedTypeHandlerClassName);
+    }
+
+    private static boolean isDelegateToCustomFieldSerializer(Class<TypeHandler> typeHandlerClass)
+    {
+        try
+        {
+            Class.forName(typeHandlerClass.getName().replaceFirst("_FieldSerializer$", "_CustomFieldSerializer"));
+            return true;
+        }
+        catch (ClassNotFoundException exception)
+        {
+            return false;
+        }
+    }
+
+    private TypeHandlerFactory() {}
+
+}

gwt-java-client/src/main/java/com/google/gwt/user/client/rpc/impl/ReflectionHelper.java
@@ -63,7 +63,7 @@ public class ReflectionHelper
         if (TypeHandler.class.isAssignableFrom(classToInstantiate))
         {
             String type = classToInstantiate.getName().replaceFirst("_FieldSerializer$", "").replace('_', '$').replaceFirst("^com.google.gwt.user.client.rpc.core.java", "java");
-            return (T) new ReflectiveTypeHandler(Class.forName(type));
+            return (T) TypeHandlerFactory.getTypeHandler(Class.forName(type));
         }
         else
         {

gwt-java-client/src/main/java/com/google/gwt/user/client/rpc/impl/ReflectiveTypeHandler.java
@@ -17,33 +17,75 @@ package com.google.gwt.user.client.rpc.impl;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 import com.google.gwt.user.client.rpc.SerializationException;
 import com.google.gwt.user.client.rpc.SerializationStreamReader;
 import com.google.gwt.user.client.rpc.SerializationStreamWriter;
 
-public class ReflectiveTypeHandler implements TypeHandler
+/**
+ * <tt>ReflectiveTypeHandler</tt> is an implementation of {@link TypeHandler} based on <tt>Java Reflection</tt> which
+ * can substitute any of standard (generated by GWT) field serializers except the ones that pin one's faith on {@link
+ * CustomFieldSerializer CustomFieldSerializers}.
+ * 
+ * <p>The implementation is thread-safe: its instances can be safely shared across multiple threads.
+*/
+class ReflectiveTypeHandler implements InvocationHandler, TypeHandler
 {
-    private static final TypeHandler NO_TYPE_HANDLER = new ReflectiveTypeHandler(null);
+    private final LinkedList<Field> fields = new LinkedList<Field>();
 
-    private static final ConcurrentMap<Class<?>, TypeHandler> cachedTypeHandlers = new ConcurrentHashMap<Class<?>, TypeHandler>();
+    private final Class<?> serializableClass;
+    private final Constructor<?> constructor;
 
-    private static final ConcurrentMap<Class<?>, Field[]> cachedClassFields = new ConcurrentHashMap<Class<?>, Field[]>();
-
-    private final Class<?> type; 
-
-    public ReflectiveTypeHandler(Class<?> type)
+    public ReflectiveTypeHandler(Class<?> serializableClass) throws SecurityException, NoSuchMethodException
     {
-        this.type = type;
+        for (Field field : serializableClass.getDeclaredFields())
+        {
+            field.setAccessible(true);
+            int fieldModifiers = field.getModifiers();
+            if (!Modifier.isStatic(fieldModifiers) && !Modifier.isTransient(fieldModifiers))
+            {
+                fields.add(field);
+            }
+        }
+        Collections.sort(fields, new Comparator<Field>()
+        {
+            @Override
+            public int compare(Field field1, Field field2)
+            {
+                return field1.getName().compareTo(field2.getName());
+            }
+        });
+
+        this.serializableClass = serializableClass;
+        constructor = serializableClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+    {
+        String methodName = method.getName();
+        if (methodName.equals("create"))
+        {
+            return create((SerializationStreamReader) args[0]);
+        }
+        if (methodName.equals("deserial"))
+        {
+            deserial((SerializationStreamReader) args[0], args[1]);
+            return null;
+        }
+        if (methodName.equals("serial"))
+        {
+            serial((SerializationStreamWriter) args[0], args[1]);
+            return null;
+        }
+        throw new RuntimeException("Unsupported method: " + method);
     }
 
     @Override
@@ -51,80 +93,7 @@ public class ReflectiveTypeHandler implements TypeHandler
     {
         try
         {
-            TypeHandler typeHandler = getTypeHandler(type);
-            if (typeHandler != NO_TYPE_HANDLER)
-            {
-                return typeHandler.create(reader);
-            }
-            Constructor<?> constructor = type.getDeclaredConstructor();
-            constructor.setAccessible(true);
             return constructor.newInstance();
-        }
-        catch (Exception exception)
-        {
-            throw new SerializationException(exception);
-        }
-    }
-
-    @Override
-    public void serial(SerializationStreamWriter writer, Object object) throws SerializationException
-    {
-        try
-        {
-            Class<? extends Object> c = object.getClass();
-            do
-            {
-                TypeHandler typeHandler = getTypeHandler(c);
-                if (typeHandler != NO_TYPE_HANDLER)
-                {
-                    typeHandler.serial(writer, object);
-                    return;
-                }
-                for (Field field : getTypeFields(c))
-                {
-                    if (field.getType() == boolean.class)
-                    {
-                        writer.writeBoolean(field.getBoolean(object));
-                    }
-                    else if (field.getType() == byte.class)
-                    {
-                        writer.writeByte(field.getByte(object));
-                    }
-                    else if (field.getType() == char.class)
-                    {
-                        writer.writeChar(field.getChar(object));
-                    }
-                    else if (field.getType() == short.class)
-                    {
-                        writer.writeShort(field.getShort(object));
-                    }
-                    else if (field.getType() == int.class)
-                    {
-                        writer.writeInt(field.getInt(object));
-                    }
-                    else if (field.getType() == long.class)
-                    {
-                        writer.writeLong(field.getLong(object));
-                    }
-                    else if (field.getType() == float.class)
-                    {
-                        writer.writeFloat(field.getFloat(object));
-                    }
-                    else if (field.getType() == double.class)
-                    {
-                        writer.writeDouble(field.getDouble(object));
-                    }
-                    else if (field.getType() == String.class)
-                    {
-                        writer.writeString((String) field.get(object));
-                    }
-                    else
-                    {
-                        writer.writeObject(field.get(object));
-                    }
-                }
-            }
-            while ((c = c.getSuperclass()) != Object.class);
         }
         catch (Exception exception)
         {
@@ -137,155 +106,117 @@ public class ReflectiveTypeHandler implements TypeHandler
     {
         try
         {
-            Class<? extends Object> c = object.getClass();
-            do
+            for (Field field : fields)
             {
-                TypeHandler typeHandler = getTypeHandler(c);
-                if (typeHandler != NO_TYPE_HANDLER)
+                if (field.getType() == boolean.class)
                 {
-                    typeHandler.deserial(reader, object);
-                    return;
+                    field.setBoolean(object, reader.readBoolean());
                 }
-                for (Field field : getTypeFields(c))
+                else if (field.getType() == byte.class)
                 {
-                    if (field.getType() == boolean.class)
-                    {
-                        field.setBoolean(object, reader.readBoolean());
-                    }
-                    else if (field.getType() == byte.class)
-                    {
-                        field.setByte(object, reader.readByte());
-                    }
-                    else if (field.getType() == char.class)
-                    {
-                        field.setChar(object, reader.readChar());
-                    }
-                    else if (field.getType() == short.class)
-                    {
-                        field.setShort(object, reader.readShort());
-                    }
-                    else if (field.getType() == int.class)
-                    {
-                        field.setInt(object, reader.readInt());
-                    }
-                    else if (field.getType() == long.class)
-                    {
-                        field.setLong(object, reader.readLong());
-                    }
-                    else if (field.getType() == float.class)
-                    {
-                        field.setFloat(object, reader.readFloat());
-                    }
-                    else if (field.getType() == double.class)
-                    {
-                        field.setDouble(object, reader.readDouble());
-                    }
-                    else if (field.getType() == String.class)
-                    {
-                        field.set(object, reader.readString());
-                    }
-                    else
-                    {
-                        field.set(object, reader.readObject());
-                    }
+                    field.setByte(object, reader.readByte());
+                }
+                else if (field.getType() == char.class)
+                {
+                    field.setChar(object, reader.readChar());
+                }
+                else if (field.getType() == short.class)
+                {
+                    field.setShort(object, reader.readShort());
+                }
+                else if (field.getType() == int.class)
+                {
+                    field.setInt(object, reader.readInt());
+                }
+                else if (field.getType() == long.class)
+                {
+                    field.setLong(object, reader.readLong());
+                }
+                else if (field.getType() == float.class)
+                {
+                    field.setFloat(object, reader.readFloat());
+                }
+                else if (field.getType() == double.class)
+                {
+                    field.setDouble(object, reader.readDouble());
+                }
+                else if (field.getType() == String.class)
+                {
+                    field.set(object, reader.readString());
+                }
+                else
+                {
+                    field.set(object, reader.readObject());
                 }
             }
-            while ((c = c.getSuperclass()) != Object.class);
         }
         catch (Exception exception)
         {
             throw new SerializationException(exception);
         }
+        if (serializableClass.getSuperclass() != Object.class)
+        {
+            TypeHandlerFactory.getTypeHandler(serializableClass.getSuperclass()).deserial(reader, object);
+        }
     }
 
-    private TypeHandler getTypeHandler(Class<?> c) throws SerializationException
+    @Override
+    public void serial(SerializationStreamWriter writer, Object object) throws SerializationException
     {
         try
         {
-            if (!cachedTypeHandlers.containsKey(c))
+            for (Field field : fields)
             {
-                List<Class<TypeHandler>> typeHandlers = getTypeHandlerClasses(c);
-                cachedTypeHandlers.putIfAbsent(c, typeHandlers.isEmpty() ? NO_TYPE_HANDLER : typeHandlers.get(0).getDeclaredConstructor().newInstance());
+                if (field.getType() == boolean.class)
+                {
+                    writer.writeBoolean(field.getBoolean(object));
+                }
+                else if (field.getType() == byte.class)
+                {
+                    writer.writeByte(field.getByte(object));
+                }
+                else if (field.getType() == char.class)
+                {
+                    writer.writeChar(field.getChar(object));
+                }
+                else if (field.getType() == short.class)
+                {
+                    writer.writeShort(field.getShort(object));
+                }
+                else if (field.getType() == int.class)
+                {
+                    writer.writeInt(field.getInt(object));
+                }
+                else if (field.getType() == long.class)
+                {
+                    writer.writeLong(field.getLong(object));
+                }
+                else if (field.getType() == float.class)
+                {
+                    writer.writeFloat(field.getFloat(object));
+                }
+                else if (field.getType() == double.class)
+                {
+                    writer.writeDouble(field.getDouble(object));
+                }
+                else if (field.getType() == String.class)
+                {
+                    writer.writeString((String) field.get(object));
+                }
+                else
+                {
+                    writer.writeObject(field.get(object));
+                }
             }
-            return cachedTypeHandlers.get(c);
         }
         catch (Exception exception)
         {
             throw new SerializationException(exception);
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    private static List<Class<TypeHandler>> getTypeHandlerClasses(Class<?> c)
-    {
-        LinkedList<Class<TypeHandler>> typeHandlers = new LinkedList<Class<TypeHandler>>();
-        do
+        if (serializableClass.getSuperclass() != Object.class)
         {
-            try
-            {
-                Class<TypeHandler> typeHandler = getGeneratedTypeHandlerClass(c);
-                if (!c.isEnum() && hasNativeMethods(typeHandler))
-                {
-                    return Collections.EMPTY_LIST;
-                }
-                typeHandlers.add(typeHandler);
-            }
-            catch (ClassNotFoundException exception)
-            {
-                break;
-            }
+            TypeHandlerFactory.getTypeHandler(serializableClass.getSuperclass()).serial(writer, object);
         }
-        while ((c = c.getSuperclass()) != Object.class);
-        return typeHandlers;
-    }
-
-    @SuppressWarnings("unchecked")
-    private static Class<TypeHandler> getGeneratedTypeHandlerClass(Class<?> c) throws ClassNotFoundException
-    {
-        String generatedTypeHandlerClassName = c.getName().replace('$', '_') + "_FieldSerializer";
-        if (generatedTypeHandlerClassName.startsWith("java"))
-        {
-            generatedTypeHandlerClassName = "com.google.gwt.user.client.rpc.core." + generatedTypeHandlerClassName;
-        }
-        return (Class<TypeHandler>) Class.forName(generatedTypeHandlerClassName);
-    }
-
-    private static boolean hasNativeMethods(Class<?> c)
-    {
-        for (Method method : c.getDeclaredMethods())
-        {
-            if (method.toString().contains(" native ")) return true; 
-        }
-        return false;
-    }
-
-    private static Field[] getTypeFields(Class<?> c)
-    {
-        if (!cachedClassFields.containsKey(c))
-        {
-            LinkedList<Field> fields = new LinkedList<Field>();  
-            for (Field field : c.getDeclaredFields())
-            {
-                field.setAccessible(true);
-
-                int fieldModifiers = field.getModifiers();
-                if (!Modifier.isStatic(fieldModifiers) && !Modifier.isTransient(fieldModifiers))
-                {
-                    fields.add(field);
-                }
-            }
-            Field[] result = fields.toArray(new Field[0]);
-            Arrays.sort(result, 0, result.length, new Comparator<Field>()
-            {
-                @Override
-                public int compare(Field field1, Field field2)
-                {
-                    return field1.getName().compareTo(field2.getName());
-                }
-            });
-            cachedClassFields.putIfAbsent(c, result);
-        }
-        return cachedClassFields.get(c);
     }
 
 }

commit 8a0568964c5af0fadeb66a0f94e1e4ccc11b61ff

Restore an ability to use Collections.EMPTY_LIST, Collections.EMPTY_MAP, Collections.EMPTY_SET (and other nested classes that have custom field serializers) as arguments and return values in GWT-RPC. The ability was broken by 38c51c6ebe99d35dd93c7df90529a0f638cbbd7d.

gwt-java-client/src/main/java/com/google/gwt/user/client/rpc/impl/TypeHandlerFactory.java
@@ -87,7 +87,7 @@ class TypeHandlerFactory
     {
         try
         {
-            Class.forName(typeHandlerClass.getName().replaceFirst("_FieldSerializer$", "_CustomFieldSerializer"));
+            Class.forName(typeHandlerClass.getName().replace('_', '$').replaceFirst("\\$FieldSerializer$", "_CustomFieldSerializer"));
             return true;
         }
         catch (ClassNotFoundException exception)

gwt-java-client/src/test/java/com/mind/gwt/jclient/GwtRpcSerializationTest.java
@@ -15,6 +15,9 @@
 */
 package com.mind.gwt.jclient;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -248,6 +251,35 @@ public class GwtRpcSerializationTest
                             }
                         });
                     }
+                });
+            }
+        }.execute();
+    }
+
+    @Test
+    public void testEmptyListTransmission() throws InterruptedException
+    {
+        new TestGwtJavaClient()
+        {
+            @Override
+            public void run()
+            {
+                final ServiceAsync service = GWT.create(Service.class);
+                service.putAndGetList(Collections.emptyList(), "[]", new SimpleAsyncCallback<List<?>>()
+                {
+                    @Override
+                    public void onSuccess(List<?> result)
+                    {
+                        if (result.isEmpty())
+                        {
+                            success();
+                        }
+                        else
+                        {
+                            failure();
+                        }
+                    }
+
                 });
             }
         }.execute();

gwt-java-client/src/test/java/com/mind/gwt/jclient/test/client/Service.java
@@ -16,6 +16,7 @@
 package com.mind.gwt.jclient.test.client;
 
 import java.util.LinkedList;
+import java.util.List;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
@@ -53,6 +54,8 @@ public interface Service extends RemoteService
     AggregatedEnumeration getAggregatedEnumeration();
 
     void putAggregatedEnumeration(AggregatedEnumeration aggregatedEnumeration);
+
+    List<?> putAndGetList(List<?> list, String reference);
 
     String getCookies();
 

gwt-java-client/src/test/java/com/mind/gwt/jclient/test/client/ServiceAsync.java
@@ -16,6 +16,7 @@
 package com.mind.gwt.jclient.test.client;
 
 import java.util.LinkedList;
+import java.util.List;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.mind.gwt.jclient.test.dto.Cookie;
@@ -51,6 +52,8 @@ public interface ServiceAsync
     void getAggregatedEnumeration(AsyncCallback<AggregatedEnumeration> callback);
 
     void putAggregatedEnumeration(AggregatedEnumeration aggregatedEnumeration, AsyncCallback<Void> callback);
+
+    void putAndGetList(List<?> list, String reference, AsyncCallback<List<?>> callback);
 
     void getCookies(AsyncCallback<String> callback);
 

gwt-java-client/src/test/java/com/mind/gwt/jclient/test/server/ServiceImpl.java
@@ -16,6 +16,7 @@
 package com.mind.gwt.jclient.test.server;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
@@ -118,7 +119,17 @@ public class ServiceImpl extends RemoteServiceServlet implements Service
         if (!aggregatedEnumeration.equals(AggregatedEnumeration.createClientToServerObject()))
         {
             throw new IllegalArgumentException();
-       }
+        }
+    }
+
+    @Override
+    public List<?> putAndGetList(List<?> list, String reference)
+    {
+        if (!list.toString().equals(reference))
+        {
+            throw new IllegalArgumentException("Got: " + list + ", but expected: " + reference);
+        }
+        return list;
     }
 
     @Override