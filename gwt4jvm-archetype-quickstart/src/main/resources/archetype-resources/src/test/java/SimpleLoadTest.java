31dbe38004208ce3601cc622761d7b86210cc791
windicted <windicted@gmail.com>
2012-06-03 18:13:06 +0400

    Clean up generated SimpleLoadTest.

--- gwt4jvm-archetype-quickstart/src/main/resources/archetype-resources/src/test/java/SimpleLoadTest.java
+++ gwt4jvm-archetype-quickstart/src/main/resources/archetype-resources/src/test/java/SimpleLoadTest.java
@@ -8,12 +8,16 @@ import org.junit.Test;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Timer;
 import com.mind.gwt.jclient.GwtJavaClient;
 import com.mind.gwt.jclient.GwtLoadTest;
 import com.mind.gwt.jclient.context.Context;
 
 public class SimpleLoadTest implements EntryPoint {
+
+    private static final int CONCURRENT_USERS = Integer.getInteger("concurrentUsers", 1);
+    private static final int RAMP_UP_SECONDS = Integer.getInteger("rampUpSeconds", 0);
+    private static final int TEST_DURATION_SECONDS = Integer.getInteger("testDurationSeconds", 0);
+    private static final String MODULE_BASE_URL = System.getProperty("moduleBaseURL");
 
     @Override
     public void onModuleLoad() {
@@ -24,13 +28,10 @@ public class SimpleLoadTest implements EntryPoint {
     public void test() throws InstantiationException, IllegalAccessException, InterruptedException {
         final AtomicLong succeed = new AtomicLong();
         final AtomicLong failure = new AtomicLong();
-        int concurrentUsers = Integer.getInteger("concurrentUsers", 1);
-        int rampUpSeconds = Integer.getInteger("rampUpSeconds", 0);
-        int testDurationSeconds = Integer.getInteger("testDurationSeconds", 0);
-        String moduleBaseURL = System.getProperty("moduleBaseURL");
-        if (moduleBaseURL == null)
-            throw new IllegalArgumentException("No module base URL was specified!");
-        new GwtLoadTest(getClass(), moduleBaseURL) {
+        if (MODULE_BASE_URL == null) {
+            throw new IllegalArgumentException("No module base URL was specified, use 'moduleBaseURL' system property to specify it!");
+        }
+        new GwtLoadTest(getClass(), MODULE_BASE_URL) {
 
             @Override
             public void onClientFinished(GwtJavaClient client) {
@@ -38,10 +39,10 @@ public class SimpleLoadTest implements EntryPoint {
                     succeed.incrementAndGet();
                 else
                     failure.incrementAndGet();
-                System.out.println(SimpleLoadTest.class.getSimpleName() + ": concurrent users: " + getConcurrentClients() + ", succeed: " + succeed.get() + ", failure: " + failure.get());
+                System.out.println(SimpleLoadTest.class.getSimpleName() + ": concurrent users: " + getConcurrentClients() + ", succeed: " + succeed.get() + ", failed: " + failure.get());
             }
 
-        }.start(concurrentUsers, rampUpSeconds, testDurationSeconds, TimeUnit.SECONDS);
+        }.start(CONCURRENT_USERS, RAMP_UP_SECONDS, TEST_DURATION_SECONDS, TimeUnit.SECONDS);
         Assert.assertTrue(true);
     }
 
